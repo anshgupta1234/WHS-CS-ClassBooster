99e40191e790a641ecbcb096e02261de18816be1
NathanRLiu <nathanliu17@gmail.com>
2020-11-05 10:13:07 -0800

    nothing

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, fields, marshal_with
+import hashlib import sha256
+
 
 app = Flask(__name__)
+app = Flask(__name__)
+api = Api(app)
+acctDB = {{"Username":"johncena123","Password" : "4fc5597c56c28a1f8079232a2736e68637634ae2"}}
+
 
 @app.route("/")
-def home():
-    return "Hello!"p
+class account(Resource):
+    def get(self,user,passw):
+    def post(self):
