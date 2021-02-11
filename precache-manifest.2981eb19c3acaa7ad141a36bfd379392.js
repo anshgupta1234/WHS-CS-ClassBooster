57cb1b4f53c09d573f2620385ce6b565528a735c
anshgupta1234 <ansh.guptaca.2004@gmail.com>
2021-02-11 09:22:55 -0800

    Updates

---
+++ precache-manifest.2981eb19c3acaa7ad141a36bfd379392.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aeca8fe9debec4c3066aa10369d303",
+    "url": "/WHS-CS-ClassBooster/index.html"
+  },
+  {
+    "revision": "279a851989abd8331b27",
+    "url": "/WHS-CS-ClassBooster/static/css/2.c98882f1.chunk.css"
+  },
+  {
+    "revision": "f99e5d581b43345e2e0b",
+    "url": "/WHS-CS-ClassBooster/static/css/main.eb4e05a4.chunk.css"
+  },
+  {
+    "revision": "279a851989abd8331b27",
+    "url": "/WHS-CS-ClassBooster/static/js/2.9641a410.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/WHS-CS-ClassBooster/static/js/2.9641a410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99e5d581b43345e2e0b",
+    "url": "/WHS-CS-ClassBooster/static/js/main.ce700caf.chunk.js"
+  },
+  {
+    "revision": "d04a39da2b8e039fcdaa",
+    "url": "/WHS-CS-ClassBooster/static/js/runtime-main.fa7d3c09.js"
+  },
+  {
+    "revision": "a189691d62239f7371530d0c0087217d",
+    "url": "/WHS-CS-ClassBooster/static/media/PlusSign.a189691d.svg"
+  },
+  {
+    "revision": "99f78968a1e10df74dabb7a63000d858",
+    "url": "/WHS-CS-ClassBooster/static/media/VerticalEllipsis.99f78968.svg"
+  },
+  {
+    "revision": "6877374d133365349eb86575e717e8ad",
+    "url": "/WHS-CS-ClassBooster/static/media/defaultProfile.6877374d.svg"
+  },
+  {
+    "revision": "3b02502b0b3abf24d9c348ec2ef1e263",
+    "url": "/WHS-CS-ClassBooster/static/media/linearGradientBackground.3b02502b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WHS-CS-ClassBooster/static/media/logo.5d5d9eef.svg"
+  }
+]);
