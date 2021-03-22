083e77b6938bdbcd6a91731419439ac719206df1
Chinmay Devadhar <cdevadhar@gmail.com>
2021-03-21 22:37:57 -0700

    Added /delete to monglue, even more minor changes

--- backend/Delete.py
+++ backend/Delete.py
@@ -1,30 +1,38 @@
+
 from flask import Flask, request, session
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import json
 import requests
 import pymongo
 from pymongo import MongoClient
-import login
 from bson import ObjectId
-BASE = "http://127.0.0.1:5000/"
+
 
 client = MongoClient("mongodb+srv://ansh:ClassBooster@cluster0.uefsc.mongodb.net/Cluster0?retryWrites=true&w=majority")
 
 app = Flask(__name__)
-@app.route('/classrooms/delete', methods=['POST'])
-def delete():
-    updateInfo = request.json
-    id = ObjectId(updateInfo["ID"])
-    userid = session["userID"]
-    print "ID: "+userid
-    myClass = client["classrooms"][userid].find_one({'_id':id})
-    client["classrooms"][userid].delete_one({'_id':id})
-    return {}
+app.secret_key = "7de9ca677c2eb20b961ee9cf8be15220"
+api = Api(app)
+resourcefields = {
+    'username' : fields.String,
+    'password' : fields.String
+}
+
+class delete(Resource):
+    def post(self):
+        updateInfo = request.json
+        id = ObjectId(updateInfo["ID"])
+        userid = session["userID"]
+        print "ID: "+userid
+        myClass = client["classrooms"][userid].find_one({'_id':id})
+        client["classrooms"][userid].delete_one({'_id':id})
+        return {"success": "true"}
+        
     
+
+
+
     
 
 
 
-if __name__ == "__main__":
-    app.secret_key = 'secret'
-    app.run()
--- backend/Monglue.py
+++ backend/Monglue.py
@@ -28,5 +28,6 @@ api.add_resource(ForgotPassPost,"/forgotpassword/<string:verifCode>")
 api.add_resource(logout,"/logout")
 api.add_resource(update, "/classrooms/update")
 api.add_resource(rename, "/classrooms/rename)
+api.add_resource(delete, "/classrooms/delete")
 
 app.run(debug = True)
