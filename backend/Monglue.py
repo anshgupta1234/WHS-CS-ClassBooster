58946a7837473aff1e13c725042c49662f03f937
NathanRLiu <nathanliu17@gmail.com>
2021-03-21 23:49:20 -0700

    fixed more of chinmays dumb errors in Monglue
    imported all the python programs, and then made it so that calling /shuffle doesn't delete the classroom

--- backend/Monglue.py
+++ backend/Monglue.py
@@ -9,6 +9,12 @@ from addclassroom import add
 from forgotPassword import forgotPassGet, ForgotPassPost
 from flask import Flask,request,render_template, session
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from Update import update
+from Rename import rename
+from delete import delete
+from shuffle import shuffle
+from getClassrooms import get
+from getClassrooms import getAll 
 
 app = Flask(__name__)
 app.secret_key = "7de9ca677c2eb20b961ee9cf8be15220"
@@ -29,7 +35,7 @@ api.add_resource(logout,"/logout")
 api.add_resource(update, "/classrooms/update")
 api.add_resource(rename, "/classrooms/rename")
 api.add_resource(delete, "/classrooms/delete")
-api.add_resource(delete, "/shuffle")
+api.add_resource(shuffle, "/shuffle")
 api.add_resource(get, "/classrooms/get")
 api.add_resource(getAll, "/classrooms/getAll")
 
