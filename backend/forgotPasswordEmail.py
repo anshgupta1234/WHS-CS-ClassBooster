1b647973ed1a631a0e6559a4d5fb26c961a925fb
NathanRLiu <nathanliu17@gmail.com>
2021-03-23 23:02:49 -0700

    Deleted large block of code, it was giving me errors

--- backend/forgotPasswordEmail.py
+++ backend/forgotPasswordEmail.py
@@ -23,23 +23,7 @@ def emailUser(userEmail,resetLink):
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = None
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+
 
     service = build('gmail', 'v1', credentials=creds)
 
